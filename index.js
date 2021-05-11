f913e1b83038af2e972138a2582e516053a2e103
GabrielVXimenes <63026126+GabrielVXimenes@users.noreply.github.com>
2021-05-11 09:15:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function home(){
+  window.location.href = "file:///C:/Users/bielg.DESKTOP-8OJVR6H/Documents/Projetos/Javascript/TCC/home.html";
+}
+
+function about(){
+  window.location.href = "file:///C:/Users/bielg.DESKTOP-8OJVR6H/Documents/Projetos/Javascript/TCC/about.html";
+}
+
+function conta(){
+  window.location.href = "file:///C:/Users/bielg.DESKTOP-8OJVR6H/Documents/Projetos/Javascript/TCC/conta.html";
+}
